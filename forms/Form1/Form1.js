1ed3d8f759df98dc13e3fbfd5c4a7b1c8fb01b4b
Jiarong Chen <jrccc12345@gmail.com>
2021-02-24 16:37:53 -0600

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,7 @@
+let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','marge','Sam']
+let withSal = petNames
+withSal.push('Sal')
+let thrid_petsName = petNames[2]
+console.log(`The third pet is named: ${thrid_petsName}`)
+console.log( `The array has ${withSal.length} in it`)
+withSal.pop(10)
